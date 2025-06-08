51ec3e945fc9918ac585abf778dbf04eace4566c
ZhouSeeTheSky <18882836313@163.com>
2025-06-08 15:16:33 +0800

    Add Conv Structure

--- Resnet/Model.py
+++ Resnet/Model.py
@@ -3,3 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class ResNet(nn.Module):
+    def __init__(self):
+        super(ResNet, self).__init__()
+
+        expansion = 4
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,)
